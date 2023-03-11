b2f4165358bfd9d25fd91b5782403565963c5f0d
deusbog <deusbogdan@gmail.com>
2023-03-11 14:58:41 +0300

    created a 2.0 version with gpt-3.5-turbo model

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ telegram_token = os.getenv("API_KEY_TELEGRAM")
 bot = Bot(token=telegram_token)
 dp = Dispatcher(bot=bot)
 
+messages = [
+    {"role": "system", "content": "You are chat-bot powered by gpt-3.5-turbo model"},
+    {"role": "user", "content": "any question"},
+    {"role": "assistant", "content": "You are assistant"}
+]
+
+
+def update(messages, role, content):
+    messages.append({"role": role, "content": content})
+    return messages
+
 
 @dp.message_handler(commands=['start'])
 async def welcome_command(message: types.Message):
@@ -22,29 +33,19 @@ async def welcome_command(message: types.Message):
 
 
 @dp.message_handler()
-async def answer(message: types.Message):
-
+async def send(message: types.Message):
+    update(messages, "user", message.text)
     openai.api_key = os.getenv("API_KEY_OPENAI")
 
-    model_engine = "text-davinci-003"
-
-    response = openai.Completion.create(
-        model=model_engine,
-        prompt=message.text,
-        max_tokens=1024,
-        temperature=1,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=1,
-        n=1,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
     )
 
-    answer_to_your_question = response.choices[0].text
-
+    await message.answer(response['choices'][0]['message']['content'])
     user_id = message.from_user.id
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
-    await message.reply(answer_to_your_question)
 
 
 if __name__ == '__main__':
