ec7ff9f9e86d225966aa19949dee6d3061729175
deusbog <deusbogdan@gmail.com>
2023-03-11 15:21:58 +0300

    added minimal style

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def welcome_command(message: types.Message):
 
 @dp.message_handler()
 async def send(message: types.Message):
-    update(messages, "user", message.text)
+    await update(messages, "user", message.text)
     openai.api_key = os.getenv("API_KEY_OPENAI")
 
     response = openai.ChatCompletion.create(
